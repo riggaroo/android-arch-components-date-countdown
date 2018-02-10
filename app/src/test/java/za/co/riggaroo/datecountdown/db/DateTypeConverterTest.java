4f7093d175e6a8fc64d07f3a2d8d755a9724ce5a
Rebecca Franks <hello@riggaroo.co.za>
2018-02-10 15:23:36 +0200

    Added DateTypeConverter test.

---
+++ app/src/test/java/za/co/riggaroo/datecountdown/db/DateTypeConverterTest.java
@@ -0,0 +1,40 @@
+package za.co.riggaroo.datecountdown.db;
+
+import org.junit.Test;
+import org.threeten.bp.LocalDateTime;
+import org.threeten.bp.ZoneOffset;
+
+import static org.junit.Assert.*;
+
+public class DateTypeConverterTest {
+
+    @Test
+    public void convertDateToLong(){
+        LocalDateTime localDateTime = LocalDateTime.ofEpochSecond(1486728000L, 0, ZoneOffset.UTC);
+
+        long timestamp = DateTypeConverter.toTimestamp(localDateTime);
+
+        assertEquals(1486728000L, timestamp);
+    }
+    @Test
+    public void convertDateToLong_DateNull_ReturnsNull(){
+        Long timestamp = DateTypeConverter.toTimestamp(null);
+
+        assertNull(timestamp);
+    }
+
+    @Test
+    public void convertLongToDate(){
+        long timestamp = 1486728000L;
+        LocalDateTime dateTime = DateTypeConverter.toDate(timestamp);
+
+        assertEquals(LocalDateTime.ofEpochSecond(1486728000L, 0, ZoneOffset.UTC), dateTime);
+    }
+    @Test
+    public void convertLongToDate_LongNull_ReturnsNull(){
+        LocalDateTime localDateTime = DateTypeConverter.toDate(null);
+
+        assertNull(localDateTime);
+    }
+
+}
